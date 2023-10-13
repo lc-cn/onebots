b92372a0d22bd83a6392aa4af248841af1f650f3
凉菜 <1659488338@qq.com>
2023-10-13 18:25:41 +0800

    fix: 更强壮的事件过滤器

--- src/service.ts
+++ src/service.ts
@@ -34,7 +34,8 @@ export namespace Service{
             key: string,
             value: any,
         ) => {
-            if(key === "$and" || key === "$or" || key === "$not") {
+            // 如果 key 为 $and、$or、$not、$nor 则递归调用
+            if(key === "$and" || key === "$or" || key === "$not" || key==='$nor') {
                 if(!value || typeof value!=='object') throw new Error("invalid filter");
                 switch (key) {
                     case "$and":
@@ -43,6 +44,8 @@ export namespace Service{
                     case "$or":
                         return Array.isArray(value)? value.some((item) => filterFn(event, key, item)) :
                             Object.entries(value).some(([key, value]) => filterFn(event, key, value));
+                    case "$nor":
+                        return !filterFn(event,'$or', value);
                     case "$not":
                         return !filterFn(event, '$and', value);
                 }
@@ -51,7 +54,20 @@ export namespace Service{
                 return value;
             }
             if (typeof value !== "object") {
+                if(key==='$regex' && typeof value==='string') return new RegExp(value).test(event[key]);
+                if(key==='$like' && typeof value==='string') return event[key].includes(value);
+                if(key==='$gt' && typeof value==='number') return event[key]>value;
+                if(key==='$gte' && typeof value==='number') return event[key]>=value;
+                if(key==='$lt' && typeof value==='number') return event[key]<value;
+                if(key==='$lte' && typeof value==='number') return event[key]<=value;
                 return value === event[key];
+            }
+            if(key==='$between' &&
+                Array.isArray(value) &&
+                value.length === 2 &&
+                value.every((item) => typeof item === 'number')){
+                const [start,end]=value;
+                return event[key]>=start && event[key]<=end;
             }
             if(Array.isArray(value)){
                 return value.includes(event[key]);
