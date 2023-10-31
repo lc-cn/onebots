d2d8c105ec836978137cfe6f3579d3f67bc74360
student_2333 <lgc2333@126.com>
2023-11-01 04:54:08 +0800

    some tweaks
    - 重写请求 params 的处理部分，修复发送群聊消息返回的 message_id 不为数字，和发送回复消息时会出现的一些问题
    - 修复调用 API 出错返回值不带 echo 的问题
    - 其他部分重构

--- src/service/V11/action/group.ts
+++ src/service/V11/action/group.ts
@@ -1,184 +1,182 @@
-import {V11} from "@/service/V11";
-import {SegmentElem} from "icqq-cq-enable/lib/utils";
-import {processMessage} from "@/service/V11/utils";
+import { V11 } from "@/service/V11"
+import { processMessage } from "@/service/V11/utils"
 import { shareMusic } from "@/service/shareMusicCustom"
-import { MusicElem } from "icqq/lib/message";
+import { MessageElem, MusicElem } from "icqq/lib/message"
 
 export class GroupAction {
-    /**
-     * 发送群聊消息
-     * @param group_id {number} 群id
-     * @param message {import('icqq').Sendable} 消息
-     * @param message_id {string} 引用的消息ID
-     */
-    async sendGroupMsg(this: V11, group_id: number, message: string | SegmentElem|SegmentElem[], message_id?: string) {
-        const msg=message_id?await this.client.getMsg(message_id):undefined
-        const {element,quote,music,share}=await processMessage.apply(this.client,[message,msg])
-        if(music) return await shareMusic.call(this.client.pickGroup(group_id), music as MusicElem)
-        if(share) return await this.client.pickGroup(group_id).shareUrl(music.data)
-        if(element.length) {
-            return await this.client.sendGroupMsg(group_id, element, quote ? await this.client.getMsg(quote.data.message_id) : undefined)
-        }
-    }
+  /**
+   * 发送群聊消息
+   * @param group_id {number} 群id
+   * @param message {MessageElem[]} 消息
+   * @param message_id {string} 引用的消息ID
+   */
+  async sendGroupMsg(this: V11, group_id: number, message: MessageElem[], message_id?: string) {
+    const msg = message_id ? await this.client.getMsg(message_id) : undefined
+    const { element, music, share } = await processMessage.apply(this.client, [message])
+    if (music) return await shareMusic.call(this.client.pickGroup(group_id), music as MusicElem)
+    if (share) return await this.client.pickGroup(group_id).shareUrl(music.data)
+    if (!element.length) throw new Error("Empty message")
+    return await this.client.sendGroupMsg(group_id, element, msg)
+  }
 
-    /**
-     * 群组踢人
-     * @param group_id {number} 群id
-     * @param user_id {number} 成员id
-     * @param reject_add_request {boolean} 是否禁止此人加群请求
-     */
-    setGroupKick(this: V11, group_id: number, user_id: number, reject_add_request?: boolean) {
-        return this.client.setGroupKick(group_id, user_id, reject_add_request)
-    }
+  /**
+   * 群组踢人
+   * @param group_id {number} 群id
+   * @param user_id {number} 成员id
+   * @param reject_add_request {boolean} 是否禁止此人加群请求
+   */
+  setGroupKick(this: V11, group_id: number, user_id: number, reject_add_request?: boolean) {
+    return this.client.setGroupKick(group_id, user_id, reject_add_request)
+  }
 
-    /**
-     * 群禁言指定人
-     * @param group_id {number} 群id
-     * @param user_id {number} 成员id
-     * @param duration {number} 禁言时长(单位：秒)
-     */
-    setGroupBan(this: V11, group_id: number, user_id: number, duration: number = 1800) {
-        return this.client.setGroupBan(group_id, user_id, duration)
-    }
+  /**
+   * 群禁言指定人
+   * @param group_id {number} 群id
+   * @param user_id {number} 成员id
+   * @param duration {number} 禁言时长(单位：秒)
+   */
+  setGroupBan(this: V11, group_id: number, user_id: number, duration: number = 1800) {
+    return this.client.setGroupBan(group_id, user_id, duration)
+  }
 
-    /**
-     * 群禁言匿名者
-     * @param group_id {number} 群id
-     * @param flag {string} 匿名者flag
-     * @param duration {number} 禁言时长(单位：秒)
-     */
-    setGroupAnonymousBan(this: V11, group_id: number, flag: string, duration: number = 1800) {
-        return this.client.setGroupAnonymousBan(group_id, flag, duration)
-    }
+  /**
+   * 群禁言匿名者
+   * @param group_id {number} 群id
+   * @param flag {string} 匿名者flag
+   * @param duration {number} 禁言时长(单位：秒)
+   */
+  setGroupAnonymousBan(this: V11, group_id: number, flag: string, duration: number = 1800) {
+    return this.client.setGroupAnonymousBan(group_id, flag, duration)
+  }
 
-    /**
-     * 群全体禁言
-     * @param group_id {number} 群id
-     * @param enable {boolean} 是否禁言
-     */
-    setGroupWholeBan(this: V11, group_id: number, enable?: boolean) {
-        return this.client.setGroupWholeBan(group_id, enable)
-    }
+  /**
+   * 群全体禁言
+   * @param group_id {number} 群id
+   * @param enable {boolean} 是否禁言
+   */
+  setGroupWholeBan(this: V11, group_id: number, enable?: boolean) {
+    return this.client.setGroupWholeBan(group_id, enable)
+  }
 
-    /**
-     * 群匿名聊天
-     * @param group_id {number} 群id
-     * @param enable {boolean} 是否开启
-     */
-    setGroupAnonymous(this: V11, group_id: number, enable?: boolean) {
-        return this.client.setGroupAnonymous(group_id, enable)
-    }
+  /**
+   * 群匿名聊天
+   * @param group_id {number} 群id
+   * @param enable {boolean} 是否开启
+   */
+  setGroupAnonymous(this: V11, group_id: number, enable?: boolean) {
+    return this.client.setGroupAnonymous(group_id, enable)
+  }
 
-    /**
-     * 设置群管
-     * @param group_id {number} 群id
-     * @param user_id {number} 成员id
-     * @param enable {boolean} true 设为管理，false 取消管理
-     */
-    setGroupAdmin(this: V11, group_id: number, user_id: number, enable?: boolean) {
-        return this.client.setGroupAdmin(group_id, user_id, enable)
-    }
+  /**
+   * 设置群管
+   * @param group_id {number} 群id
+   * @param user_id {number} 成员id
+   * @param enable {boolean} true 设为管理，false 取消管理
+   */
+  setGroupAdmin(this: V11, group_id: number, user_id: number, enable?: boolean) {
+    return this.client.setGroupAdmin(group_id, user_id, enable)
+  }
 
-    /**
-     * 设置群成员名片(成员备注)
-     * @param group_id {number} 群id
-     * @param user_id {number} 成员id
-     * @param card {string} 名片信息，不传或传空串则为 删除名片
-     */
-    setGroupCard(this: V11, group_id: number, user_id: number, card?: string) {
-        return this.client.setGroupCard(group_id, user_id, card)
-    }
+  /**
+   * 设置群成员名片(成员备注)
+   * @param group_id {number} 群id
+   * @param user_id {number} 成员id
+   * @param card {string} 名片信息，不传或传空串则为 删除名片
+   */
+  setGroupCard(this: V11, group_id: number, user_id: number, card?: string) {
+    return this.client.setGroupCard(group_id, user_id, card)
+  }
 
-    /**
-     * 设置群精华
-     * @param message_id 消息id
-     */
-    setEssenceMessage(this: V11, message_id: string) {
-        return this.client.setEssenceMessage(message_id)
-    }
-    /**
-     * 群打卡
-     * @param group_id 群id
-     */
-    sendGroupSign(this: V11, group_id: number) {
-        return this.client.pickGroup(group_id).sign()
-    }
+  /**
+   * 设置群精华
+   * @param message_id 消息id
+   */
+  setEssenceMessage(this: V11, message_id: string) {
+    return this.client.setEssenceMessage(message_id)
+  }
+  /**
+   * 群打卡
+   * @param group_id 群id
+   */
+  sendGroupSign(this: V11, group_id: number) {
+    return this.client.pickGroup(group_id).sign()
+  }
 
-    /**
-     * 移除群精华
-     * @param message_id
-     */
-    deleteEssenceMessage(this:V11, message_id: string) {
-        return this.client.removeEssenceMessage(message_id)
-    }
-    /**
-     * 设置群名
-     * @param group_id {number} 群id
-     * @param name {string} 新群名
-     */
-    setGroupName(this: V11, group_id: number, name: string) {
-        return this.client.setGroupName(group_id, name)
-    }
+  /**
+   * 移除群精华
+   * @param message_id
+   */
+  deleteEssenceMessage(this: V11, message_id: string) {
+    return this.client.removeEssenceMessage(message_id)
+  }
+  /**
+   * 设置群名
+   * @param group_id {number} 群id
+   * @param name {string} 新群名
+   */
+  setGroupName(this: V11, group_id: number, name: string) {
+    return this.client.setGroupName(group_id, name)
+  }
 
-    /**
-     * 退出指定群聊
-     * @param group_id {number} 群id
-     */
-    setGroupLeave(this: V11, group_id: number) {
-        return this.client.setGroupLeave(group_id)
-    }
+  /**
+   * 退出指定群聊
+   * @param group_id {number} 群id
+   */
+  setGroupLeave(this: V11, group_id: number) {
+    return this.client.setGroupLeave(group_id)
+  }
 
-    /**
-     * 设置群成员头衔
-     * @param group_id {number} 群id
-     * @param user_id {number} 成员id
-     * @param special_title {string} 头衔
-     * @param duration {number} 持有时长 不传则永久
-     */
-    setGroupSpecialTitle(this: V11, group_id: number, user_id: number, special_title: string, duration: number = -1) {
-        return this.client.setGroupSpecialTitle(group_id, user_id, special_title, duration)
-    }
+  /**
+   * 设置群成员头衔
+   * @param group_id {number} 群id
+   * @param user_id {number} 成员id
+   * @param special_title {string} 头衔
+   * @param duration {number} 持有时长 不传则永久
+   */
+  setGroupSpecialTitle(this: V11, group_id: number, user_id: number, special_title: string, duration: number = -1) {
+    return this.client.setGroupSpecialTitle(group_id, user_id, special_title, duration)
+  }
 
-    /**
-     * 处理加群请求
-     * @param flag {string} 加群flag
-     * @param approve {boolean} 是否同意(默认：true)
-     * @param reason {string} 拒绝理由，approve为false时有效(默认为空)
-     * @param block {boolean} 拒绝时是否加入黑名单，(默认：false)
-     */
-    setGroupAddRequest(this: V11, flag: string, approve: boolean = true, reason: string = '', block: boolean = false) {
-        return this.client.setGroupAddRequest(flag, approve, reason, block)
-    }
+  /**
+   * 处理加群请求
+   * @param flag {string} 加群flag
+   * @param approve {boolean} 是否同意(默认：true)
+   * @param reason {string} 拒绝理由，approve为false时有效(默认为空)
+   * @param block {boolean} 拒绝时是否加入黑名单，(默认：false)
+   */
+  setGroupAddRequest(this: V11, flag: string, approve: boolean = true, reason: string = "", block: boolean = false) {
+    return this.client.setGroupAddRequest(flag, approve, reason, block)
+  }
 
-    /**
-     * 获取群列表
-     */
-    async getGroupList(this: V11) {
-        return this.client.getGroupList()
-    }
+  /**
+   * 获取群列表
+   */
+  async getGroupList(this: V11) {
+    return this.client.getGroupList()
+  }
 
-    /**
-     * 获取指定群信息
-     * @param group_id
-     */
-    getGroupInfo(this: V11, group_id: number) {
-        return this.client.getGroupInfo(group_id)
-    }
+  /**
+   * 获取指定群信息
+   * @param group_id
+   */
+  getGroupInfo(this: V11, group_id: number) {
+    return this.client.getGroupInfo(group_id)
+  }
 
-    /**
-     * 获取群成员列表
-     * @param group_id
-     */
-    async getGroupMemberList(this: V11, group_id: number) {
-        return this.client.getGroupMemberList(group_id)
-    }
+  /**
+   * 获取群成员列表
+   * @param group_id
+   */
+  async getGroupMemberList(this: V11, group_id: number) {
+    return this.client.getGroupMemberList(group_id)
+  }
 
-    /**
-     * 获取指定群成员信息
-     * @param group_id
-     * @param user_id
-     */
-    getGroupMemberInfo(this: V11, group_id: number, user_id: number) {
-        return this.client.getGroupMemberInfo(group_id, user_id)
-    }
+  /**
+   * 获取指定群成员信息
+   * @param group_id
+   * @param user_id
+   */
+  getGroupMemberInfo(this: V11, group_id: number, user_id: number) {
+    return this.client.getGroupMemberInfo(group_id, user_id)
+  }
 }
--- src/service/V11/utils.ts
+++ src/service/V11/utils.ts
@@ -1,26 +1,26 @@
-import {Client, MessageElem, ShareElem} from "icqq";
-import {remove} from "@/utils";
-import {fromCqcode, fromSegment,SegmentElem} from "icqq-cq-enable/lib/utils";
-import {MusicElem, QuoteElem} from "icqq/lib/message";
+import { remove } from "@/utils"
+import { Client, MessageElem, ShareElem } from "icqq"
+import { MusicElem } from "icqq/lib/message"
 
-export async function processMessage(this: Client, message: string|SegmentElem|SegmentElem[],source?:QuoteElem):Promise<{ element: MessageElem[],music?:MessageElem,share?:ShareElem, quote?:QuoteElem }> {
-    const elements:MessageElem[] = typeof message === 'string' ? fromCqcode(message) : fromSegment(message)
-    let quote = elements.find(e => e.type === 'reply') as QuoteElem
-    if (quote) remove(elements, quote)
-    let music= elements.find(e=>e.type==='music') as MusicElem
-    if(music) {
-        remove(elements,music)
-        if(String(music.platform) === 'custom') {
-            music.platform = music['subtype'] // gocq 的平台数据存储在 subtype 内，兼容 icqq 时要求前端必须发送 id 字段
-        }
+export async function processMessage(
+  this: Client,
+  elements: MessageElem[]
+): Promise<{ element: MessageElem[]; music?: MessageElem; share?: ShareElem }> {
+  let music = elements.find((e) => e.type === "music") as MusicElem
+  if (music) {
+    remove(elements, music)
+    if (String(music.platform) === "custom") {
+      music.platform = music["subtype"] // gocq 的平台数据存储在 subtype 内，兼容 icqq 时要求前端必须发送 id 字段
     }
-    let share= elements.find(e=>e.type==='share') as ShareElem
-    if(share) remove(elements,share)
-    for(const element of elements){
-        if(['image','video','audio'].includes(element.type)){
-            if(element['file_id']?.startsWith('base64://')) element['file_id']=Buffer.from(element['file_id'].slice(9),'base64')
-            if(element['file']?.startsWith('base64://')) element['file']=Buffer.from(element['file'].slice(9),'base64')
-        }
+  }
+  let share = elements.find((e) => e.type === "share") as ShareElem
+  if (share) remove(elements, share)
+  for (const element of elements) {
+    if (["image", "video", "audio"].includes(element.type)) {
+      if (element["file_id"]?.startsWith("base64://"))
+        element["file_id"] = Buffer.from(element["file_id"].slice(9), "base64")
+      if (element["file"]?.startsWith("base64://")) element["file"] = Buffer.from(element["file"].slice(9), "base64")
     }
-    return {element:elements, quote:quote||source,share,music}
+  }
+  return { element: elements, share, music }
 }
