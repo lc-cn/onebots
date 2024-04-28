088aad1d2b3e893f2231a32e2892408768f82091
凉菜 <admin@liucl.cn>
2024-04-28 21:55:41 +0800

    fix: point error

--- src/adapters/icqq/index.ts
+++ src/adapters/icqq/index.ts
@@ -78,6 +78,14 @@ export default class IcqqAdapter extends Adapter<"icqq", Sendable> {
             },
             user_id: data.user_id || data.sender?.user_id || data.sender?.tiny_id,
         };
+        for(const key in result){
+            if(['group','friend','member','discuss'].includes) delete result[key]
+            else{
+                const value=Reflect.get(result,key)
+                if(typeof value==='function') delete result[key]
+            }
+
+        }
         if (data.source) {
             const message_id =
                 data.message_type === "group"
